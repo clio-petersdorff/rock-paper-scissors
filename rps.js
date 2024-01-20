1409a33c812fca552ff8e3db20b04f8b8f69daf3
Clio von Petersdorff <clio.petersdorff@gmail.com>
2024-01-20 13:40:09 +0000

    added function to determine winner of the game

--- rps.js
+++ rps.js
@@ -1,3 +1,4 @@
+// This function assigns the computer "rock", "paper", "scissors" randomly
 function getComputerChoice() {
     choice = (Math.random()*3)
     choice = Math.ceil(choice)
@@ -15,6 +16,7 @@ function getComputerChoice() {
     return comp_choice
 }
 
+// This function takes your input and the computers selection and compares them
 function playRound(playerSelection, computerSelection) {
 
     let win = null
@@ -25,55 +27,70 @@ function playRound(playerSelection, computerSelection) {
 
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
         console.log("Computer played "+computerSelection.toUpperCase())
-        win =  "YOU win!"
+        win =  "YOU win"
 
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper"){
         console.log("Computer: "+computerSelection.toUpperCase())
         
-        win =  "YOU win!"
+        win =  "YOU win"
 
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock"){
         console.log("Computer played "+computerSelection.toUpperCase())
-        win =  "YOU win!"
+        win =  "YOU win"
     } else {
         console.log("Computer played "+computerSelection.toUpperCase())
-        win =  "COMPUTER wins!"
+        win =  "COMPUTER wins"
     }
 
     // return "Your choice was "+playerSelection.toUpperCase()+" and the computer chose "+computerSelection.toUpperCase()+" therefore "+win
     return win
   }
    
-// const playerSelection = "rOck";
-// const computerSelection = getComputerChoice();
 
-// console.log(playRound(playerSelection, computerSelection));
 
-function game(playRound){
+// This function compares your scores and determines who the winner is 
+function determineWinner(your_score, comp_score){
+    
+    let winner = null
+
+    if (your_score > comp_score) {
+        winner = "YOU"
+    } else {
+        winner = "COMPUTER"
+    }
+
+    return winner
+
+}
+
+// This function plays allows you to play until theres a winner (at least 5 rounds)
+
+function game(playRound, determineWinner){
     let round = 0
     let your_score = 0
     let comp_score = 0
 
     while (round < 5) {
-        let playerSelection = prompt("Rock, Paper, Scissors?")
+        let playerSelection = prompt("Round " + (round + 1) +": Rock, Paper, Scissors?")
         const computerSelection = getComputerChoice();
         win = playRound(playerSelection,computerSelection)
         
-        if (win === "YOU win!"){
+        if (win === "YOU win"){
             your_score += 1
             round += 1
-        } else if (win === "you are TIED!"){
-            round += 1
+        } else if (win === "you are TIED"){
+            round = round
         } else {
             comp_score +=1
             round += 1
         }
 
-        console.log(win+" Your current score is "+your_score)
+        console.log(win+" this round! Your score: "+your_score)
     }
 
-    return your_score
+    const winner = determineWinner(your_score, comp_score)
+    return winner
 
 }
 
-console.log(game(playRound))
+console.log("GAME COMPLETE. WINNER: "+ game(playRound, determineWinner))
