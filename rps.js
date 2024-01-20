0a2ab0304467a10ba63baa712dc6c3315a5d0eef
Clio von Petersdorff <clio.petersdorff@gmail.com>
2024-01-20 13:21:57 +0000

    Added function that will keep score for 5 rounds or rock paper scissors

--- rps.js
+++ rps.js
@@ -20,22 +20,60 @@ function playRound(playerSelection, computerSelection) {
     let win = null
 
     if (playerSelection.toLowerCase() === computerSelection){
-        win = "TIED - Please play again"
+        console.log("Computer played "+computerSelection.toUpperCase())
+        win = "you are TIED"
+
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
+        console.log("Computer played "+computerSelection.toUpperCase())
         win =  "YOU win!"
+
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper"){
+        console.log("Computer: "+computerSelection.toUpperCase())
+        
         win =  "YOU win!"
+
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock"){
+        console.log("Computer played "+computerSelection.toUpperCase())
         win =  "YOU win!"
     } else {
+        console.log("Computer played "+computerSelection.toUpperCase())
         win =  "COMPUTER wins!"
     }
 
-    return "Your choice was "+playerSelection.toUpperCase()+" and the computer chose "+computerSelection.toUpperCase()+" therefore "+win
+    // return "Your choice was "+playerSelection.toUpperCase()+" and the computer chose "+computerSelection.toUpperCase()+" therefore "+win
+    return win
   }
    
-const playerSelection = "rOck";
-const computerSelection = getComputerChoice();
+// const playerSelection = "rOck";
+// const computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
 
+function game(playRound){
+    let round = 0
+    let your_score = 0
+    let comp_score = 0
+
+    while (round < 5) {
+        let playerSelection = prompt("Rock, Paper, Scissors?")
+        const computerSelection = getComputerChoice();
+        win = playRound(playerSelection,computerSelection)
+        
+        if (win === "YOU win!"){
+            your_score += 1
+            round += 1
+        } else if (win === "you are TIED!"){
+            round += 1
+        } else {
+            comp_score +=1
+            round += 1
+        }
+
+        console.log(win+" Your current score is "+your_score)
+    }
+
+    return your_score
+
+}
+
+console.log(game(playRound))
